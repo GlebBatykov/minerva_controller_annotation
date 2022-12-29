0c4b652b2cc15f4aed64c3e2bff40a1bed3727e1
Gleb Batykov <gleb.batykov@gmail.com>
2022-12-29 18:11:12 +0300

    Adds Delete annotation for Delete actions

--- lib/src/endpoint_annotation.dart
+++ lib/src/endpoint_annotation.dart
@@ -187,6 +187,32 @@ class Trace extends ActionAnnotation {
       this.filter});
 }
 
+/// Annotation class used to indicate that the controller method is an action processing DELETE requests.
+class Delete extends ActionAnnotation {
+  /// Template of the path to the action.
+  ///
+  /// The action name can be used in the template, it will be substituted automatically.
+  /// To use the action name, use {action} in its path.
+  final String path;
+
+  /// Handler for errors that occur during the operation of the action.
+  ///
+  /// It must be a top-level function or a static method of the class.
+  final EndpointErrorHandler? errorHandler;
+
+  /// Authentication settings for the endpoint.
+  final AuthOptions? authOptions;
+
+  /// Filter settings for the endpoint.
+  final RequestFilter? filter;
+
+  const Delete(
+      {this.path = '/{action}',
+      this.errorHandler,
+      this.authOptions,
+      this.filter});
+}
+
 /// The base class for annotation classes used to indicate that the controller method is the endpoint for processing websocket connections.
 abstract class WebSocketEndpointAnnotation {
   const WebSocketEndpointAnnotation();
